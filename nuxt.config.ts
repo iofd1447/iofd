1c271cc735362d6af212fa1ef7de6e1418a1cf2a
nurdjedidi <djedidinur@gmail.com>
2025-11-04 23:21:10 +0100

    'release'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,16 @@ export default defineNuxtConfig({
     },
     //...
   ],
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.ico' }
+      ]
+    }
+  },
   vite: {
     vue: {
       template: {
