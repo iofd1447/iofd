b61852ae7a00f7630153556b53ffd4cf2362c294
nurdjedidi <djedidinur@gmail.com>
2025-11-22 16:16:24 +0100

    'release'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,17 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon/android-chrome-512x512.png' },
+
+        { rel: 'manifest', href: '/favicon/site.webmanifest' }
       ]
     }
   },
